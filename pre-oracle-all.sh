82b6e0b76c867d0128269859bc266f596f1153c5
anhacvai11 <90373481+anhacvai11@users.noreply.github.com>
2024-06-14 21:09:48 +0700

    Update pre-oracle-all.sh

--- pre-oracle-all.sh
+++ pre-oracle-all.sh
@@ -8,7 +8,7 @@ sudo chmod 777 oracle.sh
 # Download and execute generate-uam.sh
 wget https://raw.githubusercontent.com/anhacvai11/utopia/main/generate-uam.sh
 sudo chmod 777 generate-uam.sh
-./generate-uam.sh 6492D886FFADE3EF7EFEE8CEF808694803B5680E9969055284120808225DA925 3
+./generate-uam.sh 6492D886FFADE3EF7EFEE8CEF808694803B5680E9969055284120808225DA925 2
 
 # Install w3m and w3m-img
 sudo apt-get install w3m w3m-img -y
