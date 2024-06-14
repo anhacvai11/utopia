98685d9b6f2069f055460a98f3294311e42e0825
anhacvai11 <90373481+anhacvai11@users.noreply.github.com>
2024-06-14 15:58:04 +0700

    Create vultr-pre-all.sh

---
+++ vultr-pre-all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Download and execute ens160-pre-install-uam.sh
+wget https://raw.githubusercontent.com/anhacvai11/utopia/main/vultr-pre-install-uam.sh
+sudo chmod 777 vultr-pre-install-uam.sh
+./vultr-pre-install-uam.sh
+
+# Download and execute generate-uam.sh
+wget https://raw.githubusercontent.com/anhacvai11/utopia/main/generate-uam.sh
+sudo chmod 777 generate-uam.sh
+./generate-uam.sh 6492D886FFADE3EF7EFEE8CEF808694803B5680E9969055284120808225DA925 2
+
+# Install w3m and w3m-img
+sudo apt-get install w3m w3m-img -y
