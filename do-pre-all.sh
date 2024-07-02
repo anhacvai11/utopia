ed1d5fb23c4f901ece6cd96ba24f4bc3053c031b
anhacvai11 <90373481+anhacvai11@users.noreply.github.com>
2024-07-02 14:12:19 +0700

    Update do-pre-all.sh

--- do-pre-all.sh
+++ do-pre-all.sh
@@ -2,12 +2,12 @@
 
 # Download and execute ens160-pre-install-uam.sh
 wget https://raw.githubusercontent.com/anhacvai11/utopia/main/do-pre-install-uam.sh
-sudo chmod 777 do-pre-install-uam.sh
+chmod 777 do-pre-install-uam.sh
 ./do-pre-install-uam.sh
 
 # Download and execute generate-uam.sh
 wget https://raw.githubusercontent.com/anhacvai11/utopia/main/generate-uam.sh
-sudo chmod 777 generate-uam.sh
+chmod 777 generate-uam.sh
 ./generate-uam.sh 7DF0D54A0C90CDB458D485A48FFB59E39EB079D3C3A0BC635414B36FEFF0380B 2
 
 # Install w3m and w3m-img
