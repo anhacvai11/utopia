fc0e938bf7e4b7ee51448683fbf42f82ea835db5
anhacvai11 <90373481+anhacvai11@users.noreply.github.com>
2024-07-21 19:28:08 +0700

    Update do-pre-all.sh

--- do-pre-all.sh
+++ do-pre-all.sh
@@ -8,7 +8,7 @@ sudo chmod 777 do-pre-install-uam.sh
 # Download and execute generate-uam.sh
 wget https://raw.githubusercontent.com/anhacvai11/utopia/main/generate-uam.sh
 sudo chmod 777 generate-uam.sh
-./generate-uam.sh 7DF0D54A0C90CDB458D485A48FFB59E39EB079D3C3A0BC635414B36FEFF0380B 2
+./generate-uam.sh 7DF0D54A0C90CDB458D485A48FFB59E39EB079D3C3A0BC635414B36FEFF0380B 2a
 
 # Install w3m and w3m-img
 sudo apt-get install w3m w3m-img -y
