6444d1b91af492b4834782009d80e758e4f83919
anhacvai11 <90373481+anhacvai11@users.noreply.github.com>
2024-10-24 13:08:39 +0700

    Update zephy.sh

--- zephy.sh
+++ zephy.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+pm2 delete all
+sudo kill $(cat ~/.pm2/pm2.pid)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
@@ -5,13 +8,11 @@ nvm install v16
 nvm use v16
 npm i pm2 -g
 pm2 delete all
-wget https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-focal-x64.tar.gz
-tar -xf xmrig-6.21.0-focal-x64.tar.gz
-cd xmrig-6.21.0
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.4/SRBMiner-Multi-2-4-4-Linux.tar.xz && tar -xf SRBMiner-Multi-2-4-4-Linux.tar.xz && cd SRBMiner-Multi-2-4-4
 rm miner.sh
 threads=$(echo $(echo "$(grep -c ^processor /proc/cpuinfo)" | bc) | awk '{printf "%d\n",$1}')
-(grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=2304" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=2304
-echo "./xmrig -o de.qrl.herominers.com:1166 -u Q0105008ceb6a34969ca84eed2d02af6560c8394c8c4bb4cfad39e234d138bed811e5bae659074e -p NA -a rx/0 -k --donate-level 1 -t $threads" >> miner.sh
+(grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=3075" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=3075
+echo "sudo ./SRBMiner-MULTI --algorithm randomx --disable-gpu --pool de-zephyr.miningocean.org:5462 --wallet ZEPHsAKWMrW7EWxhhXh3RqL6G8wSEjonejUZrKGJWR9TiVJRhMyR1wXQB5bDaSgsr5LyMPd2pnfNsKYZNC4TefttN7wkouJKPro --password NA --cpu-threads $threads" >> miner.sh
 pm2 start -f miner.sh
 pm2 log --nostream
 echo "DONE"
