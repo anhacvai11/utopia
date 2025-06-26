a3927912a064c211392f67e21023018cd315aabd
anhacvai11 <90373481+anhacvai11@users.noreply.github.com>
2025-06-27 05:15:09 +0700

    Update generate-uam.sh

--- generate-uam.sh
+++ generate-uam.sh
@@ -4,13 +4,13 @@ sudo chmod 666 /var/run/docker.sock
 
 # Kiểm tra xem tham số thứ hai (number) có được cung cấp không, nếu không thì tính toán giá trị động
 if [ -z "$2" ]; then
-  number=$(docker ps | grep tuanna9414/uam:v2 | wc -l)
+  number=$(docker ps | grep debian:bullseye-slim | wc -l)
 else
   number=$2
 fi
 
-# Xóa tất cả các container Docker dựa trên image tuanna9414/uam:v2 và xóa một thư mục
-sudo docker rm -f $(docker ps -aq --filter ancestor=tuanna9414/uam:v2) && sudo rm -rf /opt/uam_data
+# Xóa tất cả các container Docker dựa trên image debian:bullseye-slim và xóa một thư mục
+sudo docker rm -f $(docker ps -aq --filter ancestor=debian:bullseye-slim) && sudo rm -rf /opt/uam_data
 
 # Tạo tên tệp dựa trên số lượng container
 file_name=$number-docker-compose.yml
@@ -20,8 +20,8 @@ rm entrypoint*
 rm $file_name
 
 # Tải phiên bản mới của các tệp
-wget https://github.com/anhtuan9414/uam-docker/raw/master/uam-swarm/$file_name
-wget https://github.com/anhtuan9414/uam-docker/raw/master/uam-swarm/entrypoint.sh
+wget https://github.com/anhacvai11/uam-entry/tree/main/$file_name
+wget https://github.com/anhacvai11/uam-entry/tree/main/entrypoint.sh
 
 # Chạy Docker Compose với biến môi trường PBKEY được cung cấp
 sudo PBKEY=$1 docker-compose -f $file_name up -d
